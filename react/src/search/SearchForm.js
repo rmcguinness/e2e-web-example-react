4edca40c19079e1be702fd9f47b0dd266a5d2f0a
Ryan McGuinness <rmcguinness@google.com>
2023-03-12 18:34:46 -0400

    Adding comments

--- react/src/search/SearchForm.js
+++ react/src/search/SearchForm.js
@@ -6,25 +6,31 @@ import {IconButton, InputBase, Paper} from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import {tabState} from "../State";
 
+// This function creates the search form.
 export function SearchForm() {
     const [searchState, setSearchState] = useRecoilState(state.searchState);
-    const [tabValue, setTabValue] = useRecoilState(tabState);
+    const [,setTabValue] = useRecoilState(tabState);
 
+    // Handle submitting the form.
     const onSubmit = (event) => {
         event.preventDefault();
 
+        // Call the web search with a search query
         fetch("http://localhost:8088/search?query=" + searchState.query)
-            .then(res => res.json())
+            .then(res => res.json()) //Maps the response into JSON
             .then(
                 (result) => {
+                    // Add the results to the search state object, keeping the query.
                     setSearchState((s) => {
                         return {error: null, query: s.query, isReady: true, results: JSON.parse(result)};
                     })
+                    // Change the tab back to the first tab to display the results.
                     setTabValue(t => {
                         return 0;
                     })
                 },
                 (error) => {
+                    // If an error occurs, add the error to the state so it can be displayed.
                     setSearchState((s) => {
                         return {error: error, query: s.query, isReady: false, results: []};
                     })
@@ -32,18 +38,22 @@ export function SearchForm() {
             )
     }
 
+    // Store the changes to the query state, clearing the state on change,
+    // except for the updated text
     const onChange = ({target: {value}}) => {
         setSearchState((s) => {
             return {error: null, query: value, isReady: false, results: []};
         })
     }
 
+    // Handle the key on the search input, if it's enter, run the search.
     const onKeyPress = (event) => {
         if (event.key === 'Enter') {
             onSubmit(event);
         }
     }
 
+    // Creates the search input form.
     return (
         <Paper component="form">
             <InputBase
--- react/src/search/SearchResults.js
+++ react/src/search/SearchResults.js
@@ -1,19 +1,24 @@
 import {useRecoilState} from "recoil";
 import * as state from "../State";
 import Box from "@mui/material/Box";
-import {Button, Card, CardActions, CardContent} from "@mui/material";
+import {Link, Button, Card, CardActions, CardContent} from "@mui/material";
 import Typography from "@mui/material/Typography";
+import React from "react";
 
 export function SearchResults() {
     const [searchState] = useRecoilState(state.searchState);
 
+    // Iterate over the searchState.results[], which is an array of objects.
+    // for each item, create a card  that can be displayed.
     let out = searchState.results.map(function (item) {
         return (
             <Card key={item.id} sx={{margin: 2}}>
                 <CardContent>
-                    <Typography component="div" sx={{fontSize: 16}}>{item.title}</Typography>
-                    <Typography component="div" sx={{fontSize: 8}}>{item.link}</Typography>
-                    <Typography component="div" sx={{fontSize: 10}}>{item.body}</Typography>
+                    <Typography variant="h6" component={'h6'}>{item.title}</Typography>
+                    <Typography variant="caption" display="block" gutterBottom>
+                        <Link href={item.link} target="_blank">{item.link}</Link>
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>{item.body}</Typography>
                 </CardContent>
                 <CardActions>
                     <Button size="small" onClick={() => {
@@ -24,17 +29,17 @@ export function SearchResults() {
         )
     })
 
+    // Handle the state change of the searchStateObject
     if (searchState.error) {
         return <div>Error: {searchState.error.message}</div>;
     } else if (!searchState.isReady && searchState.results.length === 0) {
         return <span></span>;
     } else {
+        // Return a list of cards within a box element.
         return (
             <Box sx={{width: '100%'}}>
-                <div>
-                    <h5>Results</h5>
-                    {out}
-                </div>
+                <Typography variant="h5" component={'h5'}>Search Results</Typography>
+                {out}
             </Box>
         );
     }
